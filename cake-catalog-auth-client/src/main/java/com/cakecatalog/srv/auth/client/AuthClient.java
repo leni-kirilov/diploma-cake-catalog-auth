7cb34501e48faa2ebafc31ada6b2051becfaab3d
Leni Kirilov <Leni.Kirilov@taulia.com>
2016-12-31 14:55:03 +0200

    Run scripts README improved Client configurable with URL string

--- cake-catalog-auth-client/src/main/java/com/cakecatalog/srv/auth/client/AuthClient.java
+++ cake-catalog-auth-client/src/main/java/com/cakecatalog/srv/auth/client/AuthClient.java
@@ -4,21 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
-public class Client {
+public class AuthClient {
 
-  private static final Logger log = LoggerFactory.getLogger(Client.class);
+  private static final Logger log = LoggerFactory.getLogger(AuthClient.class);
 
-  public static boolean login(LoginRequest loginRequest) {
+  private String url = "http://localhost:8008/";
+
+  AuthClient() {}
+
+  AuthClient(String url) {
+    this.url = url;
+  }
+
+  //TODO need a client test
+  public boolean login(LoginRequest loginRequest) {
     RestTemplate restTemplate = new RestTemplate();
     Boolean isLoginSuccessful = restTemplate.postForObject(
-      "http://localhost:8080/login",
+      url + "/login",
       loginRequest,
       Boolean.class);
     return isLoginSuccessful;
   }
 
   public static void main(String args[]) {
-    boolean isSuccess = Client.login(new LoginRequest("sample@email.com", "samplePassword"));
+    boolean isSuccess = new AuthClient().login(new LoginRequest("sample@email.com", "samplePassword"));
     log.info("Login is successful?: " + isSuccess);
   }
 
---
+++ run-local.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Starting locally. 8008 and DEV"
+java $JAVA_OPTS -Dserver.port=8008 -Dspring.profiles.active=dev -jar cake-catalog-auth-server/build/libs/cake-catalog-auth-srv-0.0.1-SNAPSHOT.jar
