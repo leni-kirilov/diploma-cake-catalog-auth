c747f8e1f960326e6d2fd71f6b3e19ada2c9e1f0
Leni Kirilov <leni.kirilov@gmail.com>
2017-01-03 20:41:56 +0200

    NewRelic agent added (#6)
    - updated run scripts to utilize newRelic development vs production

--- run-local.sh
+++ run-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting locally. 8008 and DEV"
 gradle cake-catalog-auth-server:clean cake-catalog-auth-server:build
-java $JAVA_OPTS -Dserver.port=8008 -Dspring.profiles.active=dev -jar cake-catalog-auth-server/build/libs/cake-catalog-auth-srv-0.0.1-SNAPSHOT.jar
+java $JAVA_OPTS -javaagent:./cake-catalog-auth-server/newrelic/newrelic.jar -Dnewrelic.environment=development -Dserver.port=8008 -Dspring.profiles.active=dev -jar cake-catalog-auth-server/build/libs/cake-catalog-auth-srv-0.0.1-SNAPSHOT.jar
